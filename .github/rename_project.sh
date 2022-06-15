e06d8188bee0b4bec9ce4d777498714df79f26e4
jej <jej@users.noreply.github.com>
2022-06-15 13:18:59 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jej"
+original_name="test_template"
+original_urlname="test_template"
+original_description="Awesome test_template created by jej"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv test_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
